80c685f15983b243f9df5b08e755c13b48ebb34b
ShahidAfridKhan <shahidafridkhanphatan@gmail.com>
2026-06-20 16:09:46 +0530

    new change

--- src/ExceptionPractice/EgTryCatch.java
+++ src/ExceptionPractice/EgTryCatch.java
@@ -1,5 +1,32 @@
 package ExceptionPractice;
+class tryy{
+    static void tryblockA() {
+        try {
+            int a = 1;
+            int b = 0;
 
+            System.out.println(a / b);
+        } catch (Exception e) {
+            System.out.println("no it wont work that why iam here");
+        }
+    }
+}
 public class EgTryCatch {
 
+        public static void main(String args[]){
+            int a=10;
+            int b=0;
+            //it wont run so we use the try catch for the maintain the application neact and clean
+            // without the error
+           // System.out.println(a/b);
+/*
+            try{
+                System.out.println(a/b);
+            }catch(Exception e){
+                System.out.println("bro sorry this is error that can cause thats why we are useig the try catch");
+            }
+
+ */
+            tryy.tryblockA();
+        }
 }
